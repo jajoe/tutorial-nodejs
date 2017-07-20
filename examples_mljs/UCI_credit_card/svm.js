e105c72540ae3c410eff1abdbc872b374efb9525
jajoe <joseph.lefevre@insa-rouen.fr>
2017-07-20 19:09:06 +0200

    Add an example with a dataset of UCI

--- examples_mljs/UCI_credit_card/svm.js
+++ examples_mljs/UCI_credit_card/svm.js
@@ -1,16 +1,18 @@
 const SVM = require('libsvm-js/asm');
 const csv = require('csvtojson');
+const PCA = require('ml-pca');
 
+let svm;
 let options = {
-    kernel : SVM.KERNEL_TYPES.POLYNOMIAL,
-    degree : 3,
-    shrinking : false
+    kernel : SVM.KERNEL_TYPES.RBF,
+    gamma : 1e-2,
+    cost : 1,
+    shrinking : true
 }
+let pca;
 
-const csvFilePath = 'leaf.csv'; // Data
-const names = ['type', 'specimenNumber', 'eccentricity', 'aspectRatio', 'elongation', 'solidity', 'stochasticConvexity', 'isoperimetricFactor', 'maxIndetationDepth', 'lobedness', 'intensity', 'contrast', 'smoothness', 'thirdMoment', 'uniformity', 'entropy']; // For header
-
-svm = new SVM(options);
+const csvFilePath = 'data.csv'; // Data
+const names = ["ID", "LIMIT_BAL", "SEX", "EDUCATION", "MARRIAGE", "AGE", "PAY_0", "PAY_2", "PAY_3", "PAY_4", "PAY_5", "PAY_6", "BILL_AMT1", "BILL_AMT2", "BILL_AMT3", "BILL_AMT4", "BILL_AMT5", "BILL_AMT6", "PAY_AMT1", "PAY_AMT2", "PAY_AMT3", "PAY_AMT4", "PAY_AMT5", "PAY_AMT6", "label"]; // For header
 
 let seperationSize;
 let data = [], X = [], y = [];
@@ -22,7 +24,7 @@ csv({noheader: true, headers: names})
         data.push(jsonObj); // Push each object to data Array
     })
     .on('done', (error) => {
-        seperationSize = 0.9 * data.length;
+        seperationSize = 0.2 * data.length;
         data = shuffleArray(data);
         dressData();
     });
@@ -30,14 +32,14 @@ csv({noheader: true, headers: names})
 function dressData() {
     let types = new Set(); // To gather UNIQUE classes
     data.forEach((row) => {
-        types.add(row.type);
+        types.add(row.label);
     });
     typesArray = [...types]; // To save the different types of classes.
 
     data.forEach((row) => {
         let rowArray, typeNumber;
-        rowArray = Object.keys(row).map(key => parseFloat(row[key])).slice(2, 16); // We don't use the 2 first elements, which are the type (i.e class) and the specimen number (i.e ID)
-        typeNumber = typesArray.indexOf(row.type); // Convert type(String) to type(Number)
+        rowArray = Object.keys(row).map(key => parseFloat(row[key])).slice(1, 24); // We don't use the 1 first element1, which are the the ID
+        typeNumber = typesArray.indexOf(row.label); // Convert type(String) to type(Number)
 
         X.push(rowArray);
         y.push(typeNumber);
@@ -52,28 +54,32 @@ function dressData() {
 }
 
 function train() {
+    pca = new PCA(trainingSetX);
+    trainingSetX = pca.predict(trainingSetX).subMatrixColumn([0, 1]); // PCA with 2 components
+    svm = new SVM(options);
     svm.train(trainingSetX, trainingSetY);
     test();
 }
 
 function test() {
+    testSetX = pca.predict(testSetX).subMatrixColumn([0, 1]); // PCA with 2 components
     const result = svm.predict(testSetX);
-    const testSetLength = testSetX.length
+    const testSetLength = testSetX.length;
     const predictionError = error(result, testSetY);
     console.log(`Test Set Size = ${testSetLength} and number of Misclassifications = ${predictionError}`);
+    console.log((parseFloat(testSetLength - predictionError)/parseFloat(testSetLength)).toString()+" % accuracy");
 }
 
 function error(predicted, expected) {
     let misclassifications = 0;
     for (var index = 0; index < predicted.length; index++) {
-        console.log(`truth : ${expected[index]} and prediction : ${predicted[index]}`);
+        //console.log(`truth : ${expected[index]} and prediction : ${predicted[index]}`);
         if (predicted[index] !== expected[index]) {
             misclassifications++;
         }
     }
     return misclassifications;
 }
-
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
